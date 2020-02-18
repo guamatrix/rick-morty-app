313da97960f527b54433dc8782df9a62166709db
Juan Marval <juan.marval@leniolabs.com>
2020-02-18 00:18:51 -0300

    fix: Search style input

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,10 +1,24 @@
 import { useRef,  memo, useContext } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 
 import { debouce } from '../../commons/utils'
 import { Context } from '../Provider'
 import { API } from '../../commons'
 import { CharactersList } from '../../pages/api/character'
+
+
+const StyledInput = styled.input`
+  height: 30px;
+  width: 50%;
+  font-size: 14px;
+  border-radius: 4px;
+  border: 1px solid #CCCCCC;
+  
+  &:focus {
+    outline-color: #CCCCCC
+  }
+`
 
 const Search = () => {
   const searchRef = useRef<HTMLInputElement>(null)
@@ -13,7 +27,7 @@ const Search = () => {
   const onSearch = debouce(async () => {
     if (searchRef.current) {      
       try {
-        const { value } =searchRef.current
+        const { value } = searchRef.current
         const url = `${API.BASE.characters}?name=${value}`
         const { data } =  await axios.get<CharactersList>(url)
         setCharacterState(data)
@@ -23,7 +37,7 @@ const Search = () => {
     }
   }, 500, false)
   return (
-    <input ref={searchRef} onKeyUp={onSearch} />
+    <StyledInput ref={searchRef} onKeyUp={onSearch} placeholder='Search by name' />
   )
 }
 
